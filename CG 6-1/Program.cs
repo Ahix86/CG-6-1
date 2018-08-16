1d8e54f7fd9f315dfc6466d894291957bc2d28fe
Allison <allison.hix86@gmail.com>
2018-08-16 17:07:19 -0500

    homework

--- CG 6-1/Program.cs
+++ CG 6-1/Program.cs
@@ -6,7 +6,20 @@ namespace CG_6_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // ask for the users first and last names
+            // set the user input to a string value
+            Console.WriteLine("Please enter your first name");
+            string firstname = Console.ReadLine();
+            Console.WriteLine("Please enter your last name");
+            string lastname = Console.ReadLine();
+
+            //set the string builder to add the two above string into one to output the users full name
+           System.Text.StringBuilder mynewstring = new System.Text.StringBuilder(" " + firstname + " " + lastname);
+
+            Console.WriteLine("Hello," + " " + mynewstring);
+
+            Console.ReadLine();
+                  
         }
     }
 }
